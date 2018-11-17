677c2d4f5c65f82ea46e9e663e92f01817bc38df
Alex Tesevich <alex.tesevich@gmail.com>
2018-11-17 14:59:39 +0300

    Post

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,18 +75,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'djgirls_db',
-            'USER': 'postgres',
-            # 'PASSWORD': 'mypassword',
-            'HOST': '127.0.0.1',
-            'PORT': '5432',
-        }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    #         'ENGINE': 'django.db.backends.postgresql',
+    #         'NAME': 'djgirls_db',
+    #         'USER': 'postgres',
+    #         # 'PASSWORD': 'mypassword',
+    #         'HOST': '127.0.0.1',
+    #         'PORT': '5432',
+    #     }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
